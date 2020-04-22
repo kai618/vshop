b78f3c1f3e33c57e66148069a310e78c2c5a05d2
kai618 <vanhieu1601@gmail.com>
2020-04-22 17:03:40 +0700

    removed enviroments from repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/admin/products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, BsNavbarComponent],
+  declarations: [AppComponent, BsNavbarComponent, HomeComponent, ProductsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
