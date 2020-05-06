7b74a5c158c9c15aa87d48d41f7c0de9e7b48d83
kai618 <vanhieu1601@gmail.com>
2020-05-06 19:09:59 +0700

    tried to use forkJoin

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, DocumentChangeAction } from 'angularfire2/firestore';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import { map } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
+import { Observable, of, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -34,15 +34,47 @@ export class UserService {
     }
   }
 
-  getRoles(uid: string): Observable<string[]> {
-    try {
-      return this.afs
-        .collection('roles', (ref) => ref.where(uid, '==', true))
-        .snapshotChanges()
-        .pipe(map((actions) => actions.map((a) => a.payload.doc.id)));
-    } catch (error) {
-      console.error(error);
-    }
+  private getRoleList(): Observable<string[]> {
+    return this.afs
+      .collection('roles')
+      .snapshotChanges()
+      .pipe(map((actions) => actions.map((a) => a.payload.doc.id)));
+  }
+
+  getRoles(uid: string): Observable<String[]> {
+    this.afs
+      .collectionGroup('role-users', (ref) => ref.where('active', '==', true))
+      .get()
+      .subscribe((qs) => qs.docs.forEach((doc) => console.log(doc.id)));
+
+    this.afs
+      .collection('roles')
+      .snapshotChanges()
+      .pipe(
+        mergeMap((actions) =>
+          forkJoin(
+            actions.map((a) =>
+              this.afs
+                .collection('roles')
+                .doc(a.payload.doc.id)
+                .collection('role-users')
+                .doc(uid)
+                .get()
+            )
+          )
+        )
+      )
+      .subscribe((_) => console.log(_.map((d) => d.data())));
+
+    // FIXME: Try a new way to get roles, not storing uid in documents
+    return this.afs
+      .collection('roles', (ref) => ref.where(uid, '==', true))
+      .snapshotChanges()
+      .pipe(
+        map<DocumentChangeAction<unknown>[], string[]>((actions) =>
+          actions.map((a) => a.payload.doc.id)
+        )
+      );
   }
 
   isAdmin(uid: string): Observable<boolean> {
