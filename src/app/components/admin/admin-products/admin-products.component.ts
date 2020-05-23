9ae829f54f540d54ad1dccd2e54568176cb14a5c
kai618 <vanhieu1601@gmail.com>
2020-05-23 20:45:24 +0700

    changed 2 methods isAdmin and isManager

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss'],
 })
-export class AdminProductsComponent {
+export class AdminProductsComponent implements OnInit {
   products$: Observable<any>;
-  constructor(private productSv: ProductService) {
-    this.products$ = productSv.getAll();
+  constructor(private productSv: ProductService, private router: Router) {}
+
+  ngOnInit() {
+    this.products$ = this.productSv.getAll();
+  }
+
+  toEditPage(id: string) {
+    this.router.navigate(['admin/products', id]);
   }
 }
