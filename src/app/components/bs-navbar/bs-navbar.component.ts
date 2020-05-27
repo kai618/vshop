db45bcc0f6d7e574fc601f107df388ada54514b4
kai618 <vanhieu1601@gmail.com>
2020-05-27 22:06:01 +0700

    able to revoke permissions in realtime

--- src/app/components/bs-navbar/bs-navbar.component.ts
+++ src/app/components/bs-navbar/bs-navbar.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
-import { of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { of, Subscription } from 'rxjs';
+import { switchMap, combineLatest } from 'rxjs/operators';
 
 @Component({
   selector: 'bs-navbar',
@@ -14,22 +14,43 @@ export class BsNavbarComponent implements OnInit {
   isAdmin = false;
   isManager = false;
 
+  private adminSubscription: Subscription;
+  private manangerSubscription: Subscription;
+
   constructor(public authSv: AuthService, public userSv: UserService) {}
 
   ngOnInit() {
-    this.authSv.user$
-      .pipe(
-        switchMap((user) => (user ? this.userSv.isAdmin(user.uid) : of(false)))
-      )
-      .subscribe((val) => (this.isAdmin = val));
+    this.authSv.user$.subscribe((user) => {
+      if (!user) return;
+      if (this.adminSubscription) this.adminSubscription.unsubscribe();
+      if (this.manangerSubscription) this.manangerSubscription.unsubscribe();
 
-    this.authSv.user$
-      .pipe(
-        switchMap((user) =>
-          user ? this.userSv.isManager(user.uid) : of(false)
-        )
-      )
-      .subscribe((val) => (this.isManager = val));
+      this.adminSubscription = this.userSv
+        .isAdmin(user.uid)
+        .subscribe((val) => {
+          if (!val && this.isAdmin) window.location.reload();
+          this.isAdmin = val;
+        });
+
+      this.manangerSubscription = this.userSv
+        .isManager(user.uid)
+        .subscribe((val) => {
+          if (!val && this.isManager) window.location.reload();
+          this.isManager = val;
+        });
+    });
+
+    // this.authSv.user$
+    //   .pipe(
+    //     switchMap((user) => user ? this.userSv.isAdmin(user.uid) : of(false))
+    //   )
+    //   .subscribe((val) => (this.isAdmin = val));
+
+    // this.authSv.user$
+    //   .pipe(
+    //     switchMap((user) => user ? this.userSv.isManager(user.uid) : of(false))
+    //   )
+    //   .subscribe((val) => (this.isManager = val));
   }
 
   logout() {
