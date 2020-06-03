3ee46a070e402a7d8d63999b0cef54625e3b1540
Hieu Pham <vanhieu1601@gmail.com>
2020-06-03 18:05:03 +0700

    added validation to update-product-form

--- src/app/components/admin/update-product-form/update-product-form.component.ts
+++ src/app/components/admin/update-product-form/update-product-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
@@ -7,33 +7,35 @@ import { take } from 'rxjs/operators';
 import { Product } from 'src/app/interfaces/product';
 import { LoadingBarService } from 'src/app/services/loading-bar.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { User } from 'firebase';
 import { Constants } from 'src/app/constants';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-update-product-form',
   templateUrl: './update-product-form.component.html',
   styleUrls: ['./update-product-form.component.scss'],
 })
-export class UpdateProductFormComponent implements OnInit {
+export class UpdateProductFormComponent implements OnInit, OnDestroy {
   private noPhotoURL: string;
-  isPhotoUrlValid: boolean = true;
+  isPhotoUrlValid = true;
+  sameData = true;
 
   categories$: any;
-  oldProduct: Product;
-  product: Product = {};
   private id: string;
+  oldProduct: Product = {};
 
   form: FormGroup = this.fb.group({
-    title: ['', [Validators.required]],
-    category: ['', [Validators.required]],
-    price: ['', [Validators.required, Validators.min(0)]],
-    amount: ['', [Validators.required, Validators.min(0)]],
+    title: [null, [Validators.required]],
+    category: [null, [Validators.required]],
+    price: [null, [Validators.required, Validators.min(0)]],
+    amount: [null, [Validators.required, Validators.min(0)]],
     photoURL: [
-      '',
+      null,
       [Validators.required, Validators.pattern(Constants.urlRegex)],
     ],
   });
+
+  valueChangesSub: Subscription;
 
   constructor(
     private router: Router,
@@ -51,24 +53,43 @@ export class UpdateProductFormComponent implements OnInit {
 
     this.id = this.route.snapshot.paramMap.get('id');
 
+    this.setOnValueChanges();
+
     this.productSv
       .get(this.id)
       .pipe(take(1))
       .subscribe((p) => {
         this.oldProduct = p;
-        this.product = p;
         this.loadingSv.off();
         this.setFormValue();
       });
   }
 
+  ngOnDestroy() {
+    this.valueChangesSub.unsubscribe();
+  }
+
+  setOnValueChanges() {
+    this.valueChangesSub = this.form.valueChanges.subscribe((data) => {
+      if (
+        data['title'] !== this.oldProduct.title ||
+        data['price'] !== this.oldProduct.price ||
+        data['amount'] !== this.oldProduct.amount ||
+        data['category'] !== this.oldProduct.category ||
+        data['photoURL'] !== this.oldProduct.photoURL
+      )
+        this.sameData = false;
+      else this.sameData = true;
+    });
+  }
+
   setFormValue() {
     this.form.setValue({
-      title: this.product.title,
-      category: this.product.category,
-      price: this.product.price,
-      amount: this.product.amount,
-      photoURL: this.product.photoURL,
+      title: this.oldProduct.title,
+      category: this.oldProduct.category,
+      price: this.oldProduct.price,
+      amount: this.oldProduct.amount,
+      photoURL: this.oldProduct.photoURL,
     });
   }
 
@@ -86,7 +107,7 @@ export class UpdateProductFormComponent implements OnInit {
     this.loadingSv.on();
     await this.productSv.update(
       this.id,
-      this.product,
+      this.form.value as Product,
       this.oldProduct.category
     );
     this.loadingSv.off();
@@ -95,7 +116,7 @@ export class UpdateProductFormComponent implements OnInit {
 
   async remove() {
     this.loadingSv.on();
-    await this.productSv.remove(this.id, this.product.category);
+    await this.productSv.remove(this.id, this.oldProduct.category);
     this.loadingSv.off();
     this.toProductsPage();
   }
